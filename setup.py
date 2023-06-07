6d2f5b61b54e33eb12ce976c14a1f88e2a7d6d60
Musabbir Arrafi <musabbir.arrafi@gmail.com>
2023-06-07 21:37:12 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "Alcohol-Recommender-System"
+REPO_NAME = "distilary-recommender-app"
 AUTHOR_USER_NAME = "arrafi-musabbir"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = ['streamlit']
